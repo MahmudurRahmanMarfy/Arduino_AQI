a94c078c9152281038b5eb0f8d3e99b4d03a1370
MahmudurRahmanMarfy <kamlavabla@gmail.com>
2024-12-22 23:47:09 +0600

    done

--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from matplotlib.backends.backend_pdf import PdfPages
 
 # Load data from CSV
-filename = "data/perfume_sensor_readings_2024-12-22_22-23-58.csv"  # Adjust the file path if necessary
+filename = "data/alltest_sensor_readings_2024-12-22_23-10-28.csv"  # Adjust the file path if necessary
 df = pd.read_csv(filename)
 
 # Parse the 'DateTime' column to datetime
